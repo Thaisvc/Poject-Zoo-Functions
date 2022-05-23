d0bb88044f09e21d159da20038dc412964838733
thais <thaisviana081@outlook.com>
2022-05-23 16:23:07 -0300

    countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+/* eslint-disable */
 
-function countAnimals(animal) {
-  // seu código aqui
-}
-
+const countAnimals = (animal) => {
+  if (animal === undefined) {
+    const object = {};
+    species.forEach((specie) => {
+      object[specie.name] = specie.residents.length;
+    });
+    return object;
+  }
+  if (typeof animal === 'object') {
+    if (animal.sex === undefined) {
+      return species.find((specie) => specie.name === animal.specie).residents.length;
+    }
+    return species.find((specie) => specie.name === animal.specie)
+      .residents.filter((resident) => resident.sex === animal.sex).length;
+  }
+};
+console.log(countAnimals('lions'));
 module.exports = countAnimals;
