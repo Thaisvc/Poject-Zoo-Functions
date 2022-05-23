a5aa60f90cee61b2b7c26fd0a01eb44644d6b088
thais <thaisviana081@outlook.com>
2022-05-23 09:34:51 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
-const data = require('../data/zoo_data');
-
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
-
-module.exports = getSpeciesByIds;
+ /* eslint-disable */ 
+ const { species } = require('../data/zoo_data');
+ const data = require('../data/zoo_data');
+ 
+ function getSpeciesByIds(...ids) {
+   if (ids.length === 0) {
+     return [];
+   }
+   return ids.map((id) => species.find((specie) => specie.id === id));
+ }
+ console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+ module.exports = getSpeciesByIds;
