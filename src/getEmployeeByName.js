3f354cbcd8401690f0273fb5114f87b89a3c7ddc
thais <thaisviana081@outlook.com>
2022-05-23 10:40:39 -0300

    getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
+/* eslint-disable */
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName !== undefined) {
+    return data.employees.find((person) => person.firstName === employeeName || person.lastName === employeeName)
+  }
+  return {}
 }
-
+console.log(getEmployeeByName());
 module.exports = getEmployeeByName;
