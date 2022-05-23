07614d183029415af7460633246da2715a784103
thais <thaisviana081@outlook.com>
2022-05-23 15:38:37 -0300

    getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,30 @@
+/* eslint-disable */
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+
 function isManager(id) {
-  // seu código aqui
+  if ( id === stephanieId || id === olaId || id === burlId ) {
+    return true;
+  }else{
+    return false;
+
+  }
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return employees
+      .filter(({ managers }) => managers.some((id) => id === managerId))
+      .reduce((acc, { firstName, lastName }) => {
+        acc.push(`${firstName} ${lastName}`);
+        return acc;
+      }, []);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
