cc691b18efd1c34ae3ab89bbcc3f3e59451aa117
thais <thaisviana081@outlook.com>
2022-05-24 15:42:55 -0300

    calculateEntry teste 1

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,8 +1,32 @@
+/* eslint-disable */
 const data = require('../data/zoo_data');
 
+let child = 0;
+let adult = 0;
+let senior = 0;
 function countEntrants(entrants) {
-  // seu código aqui
+  return entrants.reduce((acumulador, valorAtual) => {
+    if (valorAtual.age >= 1 && valorAtual.age < 18) {
+      console.log(valorAtual.age ,'aki');
+       childs = acumulador;
+    } else if (valorAtual.age >= 18 && valorAtual.age < 50) {
+       adult = acumulador;
+    } else if (valorAtual.age >= 50) {
+       senior = acumulador;
+    }
+
+    return {child: childs, adult: adult, senior: senior  }
+  })
+
 }
+console.log(countEntrants([
+	{ name:  'Lara Carvalho', age:  3 },
+	{ name:  'Frederico Moreira', age:  3 },
+	{ name:  'Pedro Henrique Carvalho', age:  3 },
+	{ name:  'Maria Costa', age:  18 },
+	{ name:  'Núbia Souza', age:  18 },
+	{ name:  'Carlos Nogueira', age:  50 },
+]));
 
 function calculateEntry(entrants) {
 }
