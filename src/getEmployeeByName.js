8a8d25eef2a0d8ed26d14614f50248407452e4a5
thais <thaisviana081@outlook.com>
2022-05-25 10:06:57 -0300

    lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,7 +2,7 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   if (employeeName !== undefined) {
-    return data.employees.find((person) => person.firstName === employeeName || person.lastName === employeeName);
+     return data.employees.find((person) => person.firstName === employeeName || person.lastName === employeeName);
   }
   return {};
 }
